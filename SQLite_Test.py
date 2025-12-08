f7fc2e881f6314453b62bb456cc395020f8e9f67
飯田 <haruto.shin3@gmail.com>
2025-12-08 12:14:37 +0900

    SQLite_Testの変更

--- SQLite_Test.py
+++ SQLite_Test.py
@@ -1,12 +1,14 @@
 import sqlite3
 
 # 接続
-conn = sqlite3.connect("PythonApplication1/AI.db")
+conn = sqlite3.connect("AI.db")
 cursor = conn.cursor()
 
 # データの取得
 cursor.execute("""
-SELECT * FROM TestData; 
+SELECT t.テキストデータ, k.感情名
+FROM TestData t 
+JOIN Emotion k ON t.感情ID = k.感情ID
 """)
 
 rows = cursor.fetchall()  # すべての行を取得
