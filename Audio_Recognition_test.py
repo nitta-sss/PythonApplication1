dbeb40e99532ac307cc2d29b01e474e9170014e3
新田 <nics_232144@nicsO365open.onmicrosoft.com>
2025-12-08 09:38:21 +0900

    テキスト化機能の読み取り間隔変更

--- Audio_Recognition_test.py
+++ Audio_Recognition_test.py
@@ -9,8 +9,9 @@ FORMAT        = pyaudio.paInt16
 SAMPLE_RATE   = 44100        # サンプリングレート
 CHANNELS      = 1            # モノラルかバイラルか
 INPUT_DEVICE_INDEX = 0    # マイクのチャンネル
-CALL_BACK_FREQUENCY = 3      # コールバック呼び出しの周期[sec]
-
+CALL_BACK_FREQUENCY = 0.1   # コールバック呼び出しの周期[sec] 数値秒分の音をまとめて受け取る
+frames_per_buffer = int(SAMPLE_RATE * CALL_BACK_FREQUENCY)
+print(frames_per_buffer)
 
 OUTPUT_TXT_FILE = "./" + datetime.now().strftime('%Y%m%d_%H_%M') +".txt" # テキストファイルのファイル名を日付のtxtファイルにする
 
