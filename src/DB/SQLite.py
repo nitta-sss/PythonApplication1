06e6c3c6a4bccfddf12058378d7808fada2e5b8c
飯田 <haruto.shin3@gmail.com>
2025-12-09 14:45:58 +0900

    DB変更

--- src/DB/SQLite.py
+++ src/DB/SQLite.py
@@ -6,14 +6,7 @@ cursor = conn.cursor()
 
 
 cursor.execute("""
-CREATE TABLE TestData(
-テストID INT AUTO_INCREMENT PRIMARY KEY,
-発言者ID INT NOT NULL,
-テキストデータ TEXT NOT NULL,      
-感情ID INT NOT NULL,
-FOREIGN KEY (発言者ID) REFERENCES 発言者ID(Speaker),
-FOREIGN KEY (感情ID) REFERENCES 感情ID(Emotion)
-)
+ALTER TABLE TestData_new RENAME TO TestData;
 """)
 
 
