aa8d8eaf60542ba29aee46e231bfee80ab1015f5
たいち <johoisobe232109@gmail.com>
2025-12-10 11:30:29 +0900

    スクロールバー

--- main/views.py
+++ main/views.py
@@ -9,7 +9,13 @@ def index(request):
         {"sender": "bot",  "text": "だまれ"},
         {"sender": "bot",  "text": "だまれだまれだまれだまれだまれだまれ"},
     {"sender": "bot",  "text": "だまれ"},
-    
+           {"sender": "user", "text": "こんにちは！"},
+        {"sender": "bot",  "text": "リラックスしてるよ"},
+        {"sender": "user", "text": "今日は調子いい？"},
+        {"sender": "bot",  "text": "だまれ"},
+        {"sender": "bot",  "text": "だまれ"},
+        {"sender": "bot",  "text": "だまれだまれだまれだまれだまれだまれ"},
+    {"sender": "bot",  "text": "だまれ"},
     ]
 
     return render(request, 'index.html', {"messages": messages})
