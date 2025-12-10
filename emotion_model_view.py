113c9ba526f3497e7bf5e64b06155a4d6a8b36ea
ほり <a@gmail.com>
2025-12-10 10:25:51 +0900

    整理しました

--- emotion_model_view.py
+++
@@ -1,4 +0,0 @@
-from tensorflow.keras.models import load_model
-
-model = load_model("emotion_model.h5", compile=False)
-model.summary()
--- トークないざー見る.py
+++
@@ -1,7 +0,0 @@
-import pickle
-
-with open("tokenizer.pkl", "rb") as f:
-    tokenizer = pickle.load(f)
-print(tokenizer.word_index)
-
-
